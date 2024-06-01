8e0983ec801f6635a44b99963289b7033605e8dd
JabezH <hintonjabez@gmail.com>
2024-06-01 18:49:31 -0400

    bubble sort

--- bubble-sort.js
+++ bubble-sort.js
@@ -2,16 +2,35 @@
 function bubbleSort(arr) {
 
     // Iterate through the array
+    for (let i = 0; i < arr.length; i++) {
 
+      let swapped = false;
+
+       for (let j = 0; j < arr.length - 1; j++) {
       // If the current value is greater than its neighbor to the right
+      if (arr[j] > arr[j + 1]) {
         // Swap those values
+       let temp = arr[j]
+      arr[j] = arr[j + 1];
+      arr[j + 1] = temp;       
+      
 
+      swapped = true;
         // Do not move this console.log
         console.log(arr.join(","));
 
     // If you get to the end of the array and no swaps have occurred, return
-
+        }
     // Otherwise, repeat from the beginning
+      }
+
+      if (!swapped) {
+        return arr;
+
+  }
+}
+
+    return arr;
 
 }
 
